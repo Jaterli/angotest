3dcad9bfbd8fa66aecd07b0dfdd2d93aad315def
jt <jaterli@hotmail.com>
2026-08-22 12:54:57 +0200

    feat(settings): add CSRF_TRUSTED_ORIGINS configuration from environment variable

--- backend/angotest/settings.py
+++ backend/angotest/settings.py
@@ -142,6 +142,10 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:4200').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
+# CSRF
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:4200').split(',')
+
+
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
