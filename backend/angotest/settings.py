6d44c6eb73f77b50815ddaa811b78b2f2f273faa
jt <jaterli@hotmail.com>
2026-09-03 17:46:11 +0200

    fix(settings): update redis cache backend to django_redis

--- backend/angotest/settings.py
+++ backend/angotest/settings.py
@@ -223,7 +223,7 @@ LOGGING = {
 if os.getenv('ENV') == 'production':
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'BACKEND': 'django_redis.cache.RedisCache',
             'LOCATION': 'redis://127.0.0.1:6379/1',
             'OPTIONS': {
                 'CLIENT_CLASS': 'django_redis.client.DefaultClient',
